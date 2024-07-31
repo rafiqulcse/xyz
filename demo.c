af52a85774bc508ff455ecc4ac1a691ef333e9d8
rafiqulcse <rafiqulislam.cse24@gmail.com>
2024-07-31 20:59:38 +1000

    Second-Update

--- demo.c
+++ demo.c
@@ -13,6 +13,9 @@ int main()
         printf("%d is negative.", A);  
     else if (A == 0)  
         printf("%d is zero.", A);  
+        printf("%d is zero.", A); 
+        printf("%d is zero.", A); 
+        printf("%d is zero.", A); 
   
     return 0;  
 }  
